4e4f428ffb4d898f6fea42386631972aa5a94685
Joko Susilo <susilo.j8@gmail.com>
2018-01-18 11:10:25 +0700

    typo in coin component

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -31,7 +31,7 @@ const CoinCard = ({symbol, coin_name, price_usd, percent_change_24h, percent_cha
             />
             <Text>{symbol}</Text>
             <Text>{coin_name}</Text>
-            <Text>{price_usd}<Text style={bold}></Text></Text>
+            <Text>{price_usd}<Text style={bold}> $ </Text></Text>
             <Text>Change past 24 hours: {percent_change_24h}</Text>
             <Text>Change past 7 days: {percent_change_7d}</Text>
         </View>
